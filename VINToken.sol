b9930f6c35d9bacbfdba818882a6d3f798b159df
gamevinmon <coinbetdung@gmail.com>
2025-11-27 23:13:35 +0700

    Update VINToken.sol

--- VINToken.sol
+++ VINToken.sol
@@ -1,1 +1,11 @@
+/* SPDX-License-Identifier: MIT */
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract VINToken is ERC20 {
+    constructor() ERC20("VIN", "VIN") {
+        _mint(msg.sender, 100_000_000 * 10 ** decimals());
+    }
+}
 
